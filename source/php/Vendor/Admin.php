65224c833bbbca3ce2c38929a0923abef9f9ab1a
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-03-28 15:37:36 +0200

    Adds admin class for admin modifications.

---
+++ source/php/Vendor/Admin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AlgoliaFrontend;
+
+class Admin
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Enqueue required style
+     * @return void
+     */
+    public function enqueueStyles()
+    {
+
+    }
+
+    /**
+     * Enqueue required scripts
+     * @return void
+     */
+    public function enqueueScripts()
+    {
+
+    }
+}
