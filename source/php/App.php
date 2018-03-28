9ac85aec1e19d4643b1899114676b033936e4741
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-03-28 15:51:52 +0200

    Adds nag about algolia master plugin

--- source/php/App.php
+++ source/php/App.php
@@ -6,6 +6,22 @@ class App
 {
     public function __construct()
     {
+        //Require algolia master plugin
+        add_action('init', function () {
+            if (!defined('ALGOLIA_PATH')) {
+                $this->notice();
+            } else {
+                $this->init();
+            }
+        });
+    }
+
+    /**
+     * Init plugin
+     * @return void
+     */
+    public function init()
+    {
         add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
         add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
     }
@@ -16,7 +32,6 @@ class App
      */
     public function enqueueStyles()
     {
-
     }
 
     /**
@@ -25,6 +40,18 @@ class App
      */
     public function enqueueScripts()
     {
+    }
 
+    /**
+     * Requires algolia nag message
+     * @return void
+     */
+    public function notice()
+    {
+        add_action('admin_notices', function () {
+            echo '<div class="notice notice-success is-dismissible">';
+            echo '<p>' .__('Could not find algolia master plugin (Search By Algolia). Please activate this to enable the frontend modifications.', 'algolia-frontend'). '</p>';
+            echo '</div>';
+        });
     }
 }
